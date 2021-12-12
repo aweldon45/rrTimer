76856940866a18ed2178922ac03f52589aca6c20
Tony <anthony.weldon.jr@gmail.com>
2021-12-12 16:58:30 -0500

    timer commands updated with reset and list timers

--- timer2.js
+++ timer2.js
@@ -6,17 +6,20 @@ const labelLog = {};
 const intervalLog = {};
 let id;
 
+//tracks labels
 const tracker = (label, countA) => {
   labelLog[label] = countA
 }
 
+//tracks intervals
 const intervalTracker = (label, id) => {
   intervalLog[label] = id
 }
 
-
+//creates timer
 const timer = (label, countVal) => {
   let count = countVal;
+
   id = setInterval(()=> {
     count--;
     tracker(label, count)
@@ -27,29 +30,75 @@ const timer = (label, countVal) => {
   intervalTracker(label, id)
 }
 
-
+//stops timer
 const stopper = (label) => {
   clearInterval(intervalLog[label])
-  signal = 0
+  labelLog[label] = 'timer inactive';
 }
 
+//reset timer
+const resetter = (label, countB) => {
+  stopper(label);
+  timer(label, countB);
+}
+
+//grabs timers
+const intervalList = () => {
+  let intervals = Object.keys(intervalLog)
+  return intervals
+}
+
+// start timer command
 app.get('/start/:label/:val', (req,res)=> {
 
-  // Note: Cycle through the intervalLog to make sure the same name isn't already in use, note to use reset
-  let newLabel = req.params.label;
-  let startVal = req.params.val
 
-  timer(newLabel, startVal)
-  res.send(`${newLabel} timer started`);
+  let newLabel = req.params.label;
+  let startVal = req.params.val;
+  let response = `${newLabel} timer started`
+  let count = 0
+
+  let nameHolder = Object.keys(intervalLog)
+  for (let i=0;i<nameHolder.length;i++) {
+    if (nameHolder[i] === newLabel) {
+      count = 1
+      response = 'this timer already exists, to reset it please use the reset command';
+      break;
+    } else {
+      response = `${newLabel} timer started`;
+    }
+  }
+
+  if (count < 1) {
+    timer(newLabel, startVal)
+  }
+
+  res.send(response);
 })
 
+//stop timer command
 app.get('/stop/:stopLabel', (req,res)=> {
   let label = req.params.stopLabel ;
   stopper(label)
   res.send('timer stopped');
 })
 
+//reset command
+app.get('/reset/:label/:val', (req, res) => {
+  let label = req.params.label;
+  let resetVal = req.params.val;
 
+  resetter(label, resetVal);
+  res.send(`${label} reset to start at ${resetVal}`)
+
+})
+
+//get all timers
+app.get('/listTimers', (req, res)=> {
+  let timers = intervalList()
+  res.send(`${JSON.stringify(timers)}\n\n`)
+})
+
+//get current time command
 app.get(`/currentTime/:label`, (req,res)=> {
 
   res.setHeader('Cache-Control', 'no-cache');
